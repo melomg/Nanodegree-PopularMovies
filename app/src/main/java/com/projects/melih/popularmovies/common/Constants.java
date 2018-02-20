e95dc3e8c7a24a4297c2e68ec51f1084d32dbdf0
Melih Gültekin <mmelihgultekin@gmail.com>
2018-02-20 22:20:05 +0300

    adds base urls to build gradle file, creates constants class
    Constants class contains Movie db api_key constant but need to be assigned by examiners, therefore it is leaved as TODO

---
+++ app/src/main/java/com/projects/melih/popularmovies/common/Constants.java
@@ -0,0 +1,14 @@
+package com.projects.melih.popularmovies.common;
+
+/**
+ * Created by Melih Gültekin on 20.02.2018
+ */
+
+public class Constants {
+    private Constants() {
+        // no-op
+    }
+
+    //TODO enter api key
+    public static final String MOVIE_DB_API_KEY = "";
+}
