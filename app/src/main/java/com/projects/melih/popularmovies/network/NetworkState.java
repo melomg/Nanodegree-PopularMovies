7b1b5b1c991e7d34541c28778b4309f44a54fab4
Melih Gültekin <mmelihgultekin@gmail.com>
2018-03-31 20:11:29 +0300

    adds FAILED and EMPTY states

--- app/src/main/java/com/projects/melih/popularmovies/network/NetworkState.java
+++ app/src/main/java/com/projects/melih/popularmovies/network/NetworkState.java
@@ -6,23 +6,28 @@ import android.support.annotation.Nullable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-import static com.projects.melih.popularmovies.network.State.*;
-
 /**
  * Created by Melih Gültekin on 24.02.2018
  */
 
 public class NetworkState {
-    public static final NetworkState LOADED = new NetworkState(State.SUCCESS);
-    public static final NetworkState LOADING = new NetworkState(State.RUNNING);
-    public static final NetworkState NO_NETWORK = new NetworkState(State.NO_NETWORK);
+    private static final int STATE_SUCCESS = 0;
+    private static final int STATE_RUNNING = STATE_SUCCESS + 1;
+    private static final int STATE_FAILED = STATE_RUNNING + 1;
+    private static final int STATE_NO_NETWORK = STATE_FAILED + 1;
+    private static final int STATE_EMPTY = STATE_NO_NETWORK + 1;
+
+    public static final NetworkState LOADED = new NetworkState(STATE_SUCCESS);
+    public static final NetworkState LOADING = new NetworkState(STATE_RUNNING);
+    public static final NetworkState FAILED = new NetworkState(STATE_FAILED);
+    public static final NetworkState NO_NETWORK = new NetworkState(STATE_NO_NETWORK);
+    public static final NetworkState EMPTY = new NetworkState(STATE_EMPTY);
 
     @SuppressWarnings({"FieldCanBeLocal", "unused"})
     @State
     private final int state;
     private final String errorMessage;
 
-    @SuppressWarnings("WeakerAccess")
     public NetworkState(@State int state) {
         this(state, null);
     }
@@ -38,20 +43,17 @@ public class NetworkState {
     }
 
     public static NetworkState error(@Nullable String errorMessage) {
-        return new NetworkState(State.FAILED, errorMessage);
+        return new NetworkState(STATE_FAILED, errorMessage);
     }
-}
 
-@Retention(RetentionPolicy.SOURCE)
-@IntDef(value = {
-        SUCCESS,
-        RUNNING,
-        FAILED,
-        NO_NETWORK
-})
-@interface State {
-    int SUCCESS = 0;
-    int RUNNING = SUCCESS + 1;
-    int FAILED = RUNNING + 1;
-    int NO_NETWORK = FAILED + 1;
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef(value = {
+            STATE_SUCCESS,
+            STATE_RUNNING,
+            STATE_FAILED,
+            STATE_NO_NETWORK,
+            STATE_EMPTY
+    })
+    public @interface State {
+    }
 }
