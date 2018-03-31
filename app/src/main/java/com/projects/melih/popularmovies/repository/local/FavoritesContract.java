e0296a7192605d83160c36e1544e9af2e31b4d7e
Melih Gültekin <mmelihgultekin@gmail.com>
2018-03-31 20:13:18 +0300

    adds db helper, content provider and contract class

---
+++ app/src/main/java/com/projects/melih/popularmovies/repository/local/FavoritesContract.java
@@ -0,0 +1,37 @@
+package com.projects.melih.popularmovies.repository.local;
+
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class FavoritesContract {
+    public static final String CONTENT_AUTHORITY = "com.projects.melih.popularmovies";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_FAVORITES = "favorites";
+
+    private FavoritesContract() {
+        //no-op
+    }
+
+    public static final class FavoriteEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_FAVORITES).build();
+
+        public static final String TABLE_NAME = "favorites";
+        public static final String COLUMN_MOVIE_ID = "movie_id";
+        public static final String COLUMN_MOVIE_TITLE = "movie_title";
+        public static final String COLUMN_MOVIE_SYNOPSIS = "movie_synopsis";
+        public static final String COLUMN_MOVIE_RELEASE_DATE = "movie_release_date";
+        public static final String COLUMN_MOVIE_AVERAGE_RATE = "movie_average_rate";
+        public static final String COLUMN_MOVIE_POSTER_PATH = "movie_poster_path";
+        public static final String COLUMN_MOVIE_BACKDROP_PATH = "movie_backdrop_path";
+
+        private FavoriteEntry() {
+            //no-op
+        }
+
+        public static Uri buildFavoritesUriWithId(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+}
---
+++ app/src/main/java/com/projects/melih/popularmovies/repository/local/FavoritesDbHelper.java
@@ -0,0 +1,38 @@
+package com.projects.melih.popularmovies.repository.local;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class FavoritesDbHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "favoritesDb.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TEXT_NOT_NULL = " TEXT NOT NULL";
+    private static final String REAL_NOT_NULL = " REAL NOT NULL";
+    private static final String COMMA_AND_SPACE = ", ";
+
+    // Constructor
+    public FavoritesDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String CREATE_TABLE = "CREATE TABLE " + FavoritesContract.FavoriteEntry.TABLE_NAME + " (" +
+                FavoritesContract.FavoriteEntry.COLUMN_MOVIE_ID + " INTEGER PRIMARY KEY, " +
+                FavoritesContract.FavoriteEntry.COLUMN_MOVIE_TITLE + TEXT_NOT_NULL + COMMA_AND_SPACE +
+                FavoritesContract.FavoriteEntry.COLUMN_MOVIE_SYNOPSIS + TEXT_NOT_NULL + COMMA_AND_SPACE +
+                FavoritesContract.FavoriteEntry.COLUMN_MOVIE_RELEASE_DATE + TEXT_NOT_NULL + COMMA_AND_SPACE +
+                FavoritesContract.FavoriteEntry.COLUMN_MOVIE_AVERAGE_RATE + REAL_NOT_NULL + COMMA_AND_SPACE +
+                FavoritesContract.FavoriteEntry.COLUMN_MOVIE_POSTER_PATH + TEXT_NOT_NULL + COMMA_AND_SPACE +
+                FavoritesContract.FavoriteEntry.COLUMN_MOVIE_BACKDROP_PATH + TEXT_NOT_NULL + ");";
+
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + FavoritesContract.FavoriteEntry.TABLE_NAME);
+        onCreate(db);
+    }
+}
